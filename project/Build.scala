bdada93eecce186fae06530b97e1d30b7525a884
TheBizzle <jason.bertsche@gmail.com>
2013-07-17 16:03:05 -0500

    Tuning up build file

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,13 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "ScalaJS-Sandbox"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName    = "ScalaJS-Sandbox"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
     jdbc,
     anorm
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
